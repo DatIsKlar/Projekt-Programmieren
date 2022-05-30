fce90304fc46a1777a9e28ad0c7456f6c291ad51
DatIsKlar <46826117+DatIsKlar@users.noreply.github.com>
2022-05-30 19:42:10 +0200

    Add files via upload

--- Supplier.cpp
+++ Supplier.cpp
@@ -47,22 +47,22 @@ bool Supplier::editOrderById(unsigned int idIn, Order edit){
 bool Supplier::removerOrderById(unsigned int idIn){
 	int x = this->getOrderPosById(idIn);
 	if(x>=0){
-		this->order.erase(this->order.begin()+x);
+		this->order.erase(this->order.begin()+x); // @suppress("Invalid arguments")
 		return true;
 	}
 	else
 		return false;
 }
 
-void Supplier::printOrder(){
-	std::cout<<"Legierung	"<<"Namen	"<<"Menge	"<<"Id"<<std::endl;
-
-	for(auto i: this->order){
-		std::cout<<this->alloy.getCopper()<<"	"<<i.getFirmName()<<"	"<<i.getAmount()<<"	"<<i.getId()<<std::endl;
-		std::cout<<this->alloy.getZinc()<<std::endl;
-		std::cout<<this->alloy.getTin()<<std::endl;
-	}
-}
+//void Supplier::printOrder(){
+//	std::cout<<"Legierung	"<<"Namen	"<<"Menge	"<<"Id"<<std::endl;
+//
+//	for(auto i: this->order){
+//		std::cout<<this->alloy.getCopper()<<"	"<<i.getFirmName()<<"	"<<i.getAmount()<<"	"<<i.getId()<<std::endl;
+//		std::cout<<this->alloy.getZinc()<<std::endl;
+//		std::cout<<this->alloy.getTin()<<std::endl;
+//	}
+//}
 //bool Supplier::saveStorage(std::string filename, std::string spacer) {
 //
 //	std::ofstream myfile;
