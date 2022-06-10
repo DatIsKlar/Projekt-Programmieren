3b362a2ed50625a0146c64e35b30fdc14e21c686
DatIsKlar <46826117+DatIsKlar@users.noreply.github.com>
2022-06-10 16:32:54 +0200

    Add files via upload

--- Programmierpraktikum.cpp
+++ Programmierpraktikum.cpp
@@ -5,58 +5,6 @@
 #include "Data.h"
 #include "Functions.h"
 
-
-//
-
-//std::vector<Supplier> a;
-//
-//int main() {
-//
-//
-////functions bps. functions::readSupplierFile("test",";") wurde einen array mit auﬂ Supplier mit den dataen aus der datei test welche spacer ; benutzt wiedergeben
-//	for(auto i : data::getHeaderText(';',"test")){
-//		std::cout<<i<<" ";
-//	}
-//
-//
-//	std::cout<<functions::isInt(-2);
-////	std::vector<arma::vec> a_; //3D Vector im Vector Array def
-////	arma::mat A(0, 0); //0x0 Matrix zu def
-////	Alloy aA(0.8, 0.2, 0, "name", 5); //test alloys
-////	Alloy bA(0.95, 0.0, 0.05, "test", 5);
-////	Alloy cA(0.8, 0.1, 0.1, "better", 5);
-////	Storage a;
-////	Alloy wanted(0.9, 0.05, 0.05, "test2", 5);
-////	a.addAlloy(aA);
-////	a.addAlloy(bA);
-////	a.addAlloy(cA);
-////	a.editAlloyByType(bA, Alloy(0.9, 0.1, 0.1, "better2", 5));
-////	a.removeAlloyByType(bA);
-////	Supplier ac;
-////	std::vector<std::string> search = {"Copper","Zinc","Tin","Name","Amount"};
-////	std::vector<std::string> da_ = data::getTextData("test",";",search);
-//
-////	for(auto i: da_){
-////		std::cout<<i<<std::endl;
-////	}
-////
-////	for(auto i:a.getAlloys()){
-////		std::cout<<i.getName()<<std::endl;
-////	}
-////	a.editAlloyByName("test",Alloy(.8,0.1,0.1, "better2",5));
-////	unsigned int x1 = a.getAlloyPosByName("better2");
-////	std::cout<<a.getAlloys().at(x1).getCopper();
-//
-//	//a.saveStorage("test", ";"); //Lager read/save test;
-////	Storage b;
-////	b.readStorage("test", ";");
-////	for (auto i : b.getAlloys()) {
-////		std::cout << i.getCopper() << "	" << i.getName() << std::endl;
-////	}
-//
-//	return 0;
-//}
-
 int main() {
 
 		/**
