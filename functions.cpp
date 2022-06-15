8d78e2463e94294b9373728a6b5958e16801a442
DatIsKlar <46826117+DatIsKlar@users.noreply.github.com>
2022-06-15 10:42:33 +0200

    Add files via upload
    Funktionen zum Lesen vom Liefer jetzt auch da.

--- functions.cpp
+++ functions.cpp
@@ -76,5 +76,21 @@ bool saveSupplier(std::string filename, std::string spacer,
 	return true;
 }
 
+bool readSupplier(std::string filename, std::string spacer,std::vector<Supplier> &suppliers){
+	std::vector<std::string> search = {"Kupfer","Zink","Zinn","Name","Menge","Firmname","Strasse","Plz","Stadt","Kommentar"};
+	std::vector<std::string> data_ = data::getTextData(filename, spacer, search);
+
+	for(unsigned int x = 0; x<data_.size(); x+=search.size()){
+		if(x%search.size() == 0){
+			try{
+				Alloy a(std::stof(data_.at(x)), std::stof(data_.at(x+1)), std::stof(data_.at(x+2)), data_.at(x+3), std::stof(data_.at(x+4)));
+				suppliers.push_back(Supplier(a,data_.at(x+5),data_.at(x+6),data_.at(x+7),data_.at(x+8),data_.at(x+9)));
+			}catch(const std::invalid_argument& ia){
+				return false;
+			}
+		}
+	}
+	return true;
+}
 }
 
