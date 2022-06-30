8728605cd4bea87602a74193935159997edd25dd
DatIsKlar <46826117+DatIsKlar@users.noreply.github.com>
2022-06-30 11:16:36 +0200

    Add files via upload

--- Filesave.h
+++ Filesave.h
@@ -12,7 +12,7 @@
 namespace filesave{
 
 /**
- * @fn bool saveSupplier(std::string filename, std::string spacer, std::vector<Supplier> sup)
+ * @fn bool readSupplier(std::string filename, std::string spacer, std::vector<Supplier> &suppliers)
  * Speichert ein vector vom typ Supplier in einer csv datei.
  * Gibt true wieder wenn erfolgreich and false wenn nicht.
  * @param std::string filename
@@ -39,24 +39,28 @@ bool saveSupplier(std::string filename, std::string spacer, std::vector<Supplier
 
 
 /**
- * @fn bool saveStorage(std::string nameIn, std::string spacerIn)
+ * @fn bool saveStorage(std::string nameIn, std::string spacerIn, Storage lager)
  * Speichert das Lager in einer csv Datei;
  * Gibt wahr wider wenn beendet wurde
- * @param nameIn
+ * @param std::string nameIn
  * gew�nschter Dateiname
- * @param spacerIn
+ * @param std::string spacerIn
  * gew�nschter spacer in der Datei
+ * @param Storage lager
+ * lager welches gespeichert werden soll
  */
 bool saveStorage(std::string nameIn, std::string spacerIn, Storage lager);
 
 /**
- * @fn bool readStorage(std::string filname, std::string spacerIn)
+ * @fn bool readStorage(std::string filname, std::string spacerIn,Storage &lager))
  * Liest  das Lager aus einer csv Datei und gibt es in das Lager zur�ck
  * Gibt wahr wider wenn beendet wurde
- * @param filname
+ * @param std::string filname
  * der zu �ffnende Dateiname
- * @param spacerIn
+ * @param std::string spacerIn
  * spacer in der Datei
+ * @param Storage &lager
+ * Referenz des Lagers zu welchem gelesen werden soll.
  */
 bool readStorage(std::string filename, std::string spacerIn, Storage &lager);
 }
